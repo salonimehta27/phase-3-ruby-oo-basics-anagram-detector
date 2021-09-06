799bee05873c1c5190cc3b630bd98b54d72ca2ab
Saloni <salonimehta2127@gmail.com>
2021-09-05 21:42:23 -0400

    Completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+ 
+ def initialize(word)
+    @word=word
+ end
+ attr_reader:word
+
+ def match(array)
+    array.select {|x| x.chars.sort==@word.chars.sort}
+ end
+end
+
